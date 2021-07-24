fb391ad47971884385c3252eda3021c6769a6911
ericgaspar <junk.eg@free.fr>
2021-07-24 09:23:06 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
+YNH_PHP_VERSION="7.3"
+
 # dependencies used by the app
-pkg_dependencies="php-zip php-curl php-gd"
+pkg_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd"
 
 #=================================================
 # PERSONAL HELPERS
@@ -33,10 +35,10 @@ myynh_set_permissions () {
 	[ $(find "$final_path" -type d | wc -l) -gt 0 ] && find "$final_path" -type d | xargs chmod 0755
 	[ $(find "$data_path" -type f | wc -l) -gt 0 ] && find "$data_path" -type f | xargs chmod 0644
 	[ $(find "$data_path" -type d | wc -l) -gt 0 ] && find "$data_path" -type d | xargs chmod 0755
-	chown -R root:"$app" "$final_path"
-	chown -R "$app": "$final_path/private"
-	chown -R "$app": "$data_path"
-	chown root: "$data_path"
+	chown -R $app:www-data "$final_path"
+	chmod 750 "$final_path"
+	chmod -R o-rwx "$final_path"
+	chown -R $app:www-data "$data_path"
 }
 
 #Convert --data to --data-urlencode before ynh_local_curl
